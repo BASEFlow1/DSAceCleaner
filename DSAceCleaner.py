bdb95105b6ba4bb3d7f8f44d8d69a41e1193a8bb
BASEFlow1 <67925673+BASEFlow1@users.noreply.github.com>
2021-08-19 19:46:34 +0200

    reworked the parser function
    much less bulky and much more readable now

--- DSAceCleaner.py
+++ DSAceCleaner.py
@@ -1,338 +1,82 @@
 import os
 import configparser
 import time
-def parser(inputFile, boolean):
-    ree = False
+def parser(inputFile, includeTimestamps):
+    configPresent = False
+    exclusionsPresent = False
+    exclusions = []
+    tryExcept = []
+    nameList = []       # returns
+    contentList = []    # returns
     config = configparser.ConfigParser()
     config.read('config.ini')
     if not config.read('config.ini') == []:
+        configPresent = True
+
+    if configPresent:
         if not config['EXCLUSIONS']['exclusions'] == "":
+            exclusionsPresent = True
+
+    if exclusionsPresent:
+        try:
+            exclusions = config['EXCLUSIONS']['exclusions'].split(", ")
+        except:
+            exclusions = config['EXCLUSIONS']['exclusions']
+
+    try:
+        with open(inputFile, 'r') as input:
+            input = input.read()
+            chatlog = input.split('\n')
+    except:
+        raise
+
+    i = 0
+
+    for x in chatlog:
+        if includeTimestamps:
+            kek = x
+        else:
+            kek = x[22:]
+        if ":" in kek:
             try:
-                exclusions = config['EXCLUSIONS']['exclusions'].split(", ")
-            except:
-                exclusions = config['EXCLUSIONS']['exclusions']
-            if not boolean:
-                if not ree:
-                    try:
-                        with open(inputFile, 'r') as input:
-                            input = input.read()
-                            input = input.split('\n')
-                    except:
-                        raise(FileNotFoundError)
-                    #doneAlready = []
-                    tryExcept = []
-                    nameList = []
-                    contentList = []
-                    i = 0
+                hehe = kek.split(': ', 1)
+                tryExcept.append(hehe[1])
+                works = True
+            except IndexError:
+                works = False
 
-                    for x in input:
-                        kek = x[22:]
-                        if ":" in kek:
-                            # if i not in doneAlready:
-                            #     print("Proccessing line: " +  str(i))
-                            # doneAlready.append(i)
-                            try:
-                                hehe = kek.split(': ', 1)
-                                tryExcept.append(hehe[1])
-                                works = True
-                            except IndexError:
-                                works = False
-
-                            if works:
-                                ex = 0
-                                for exclusion in exclusions:
-                                    if exclusion[-3:] == "[0]":
-                                        if not exclusion[:-3] in hehe[0]:
-                                            ex += 1
-                                    elif exclusion[-3:] == "[1]":
-                                        if not exclusion[:-3] in hehe[1]:
-                                            ex += 1
-                                    elif exclusion[-3:] == "[2]":
-                                        if not exclusion[:-3] in hehe[0]:
-                                            if not exclusion[:-3] in hehe[1]:
-                                                ex += 1
-                                    elif exclusion[-3:] == "[3]":
-                                        if not exclusion[:-3] in hehe[1][:len(exclusion)]:
-                                            ex += 1
-                                    else:
-                                        print("Invalid exclusion formatting. Each exclusion must end with either '[0]', '[1]', '[2]' or '[3]'. \n   [0] = Exclude from Name List \n   [1] = Exclude from Content List \n   [2] = Exclude from both Lists \n   [3] = Exclude if at the beginning of Content List. Used for ooRP messages.")
-                                        os._exit(0)
-                                    if ex >= len(exclusions):
-                                        nameList.append(hehe[0])
-                                        contentList.append(hehe[1])
-
-                        i = i + 1
-                        ree = True
-            else:
-                if not ree:
-                    try:
-                        with open(inputFile, 'r') as input:
-                            input = input.read()
-                            input = input.split('\n')
-                    except:
-                        raise(FileNotFoundError)
-                    #doneAlready = []
-                    tryExcept = []
-                    nameList = []
-                    contentList = []
-                    i = 0
-
-                    for x in input:
-                        kek = x
-                        if ":" in x:
-                            # if i not in doneAlready:
-                            #     print("Proccessing line: " +  str(i))
-                            # doneAlready.append(i)
-                            try:
-                                hehe = kek.split(': ', 3)
-                                tryExcept.append(hehe[1])
-                                works = True
-                                tryExcept = []
-                            except IndexError:
-                                works = False
-
-                            if works:
-                                ex = 0
-                                for exclusion in exclusions:
-                                    if exclusion[-3:] == "[0]":
-                                        if not exclusion[:-3] in hehe[0]:
-                                            ex += 1
-                                    elif exclusion[-3:] == "[1]":
-                                        if not exclusion[:-3] in hehe[1]:
-                                            ex += 1
-                                    elif exclusion[-3:] == "[2]":
-                                        if not exclusion[:-3] in hehe[0]:
-                                            if not exclusion[:-3] in hehe[1]:
-                                                ex += 1
-                                    elif exclusion[-3:] == "[3]":
-                                        if not exclusion[:-3] in hehe[1][:len(exclusion)]:
-                                            ex += 1
-                                    else:
-                                        print("Invalid exclusion formatting. Each exclusion must end with either '[0]', '[1]', '[2]' or '[3]'. \n   [0] = Exclude from Name List \n   [1] = Exclude from Content List \n   [2] = Exclude from both Lists \n   [3] = Exclude if at the beginning of Content List. Used for ooRP messages.")
-                                        os._exit(0)
-
-                                    if ex >= len(exclusions):
-                                        nameList.append(hehe[0])
-                                        contentList.append(hehe[1])
-                        i = i + 1
-                        ree = True
-        else:
-            print("No exclusions defined.")
-        if not boolean:
-            if not ree:
-                try:
-                    with open(inputFile, 'r') as input:
-                        input = input.read()
-                        input = input.split('\n')
-                except:
-                    raise(FileNotFoundError)
-                #doneAlready = []
-                tryExcept = []
-                nameList = []
-                contentList = []
-                i = 0
-
-                for x in input:
-                    kek = x[22:]
-                    if ":" in kek:
-                        # if i not in doneAlready:
-                        #     print("Proccessing line: " +  str(i))
-                        # doneAlready.append(i)
-                        try:
-                            hehe = kek.split(': ', 1)
-                            tryExcept.append(hehe[1])
-                            works = True
-                        except IndexError:
-                            works = False
-
-                        if works:
-                            if not "Death" in hehe[0]:
-                                if not "has requested to dock" in hehe[1]:
-                                    if not "Options" in hehe[0]:
-                                        if not "Tip" in hehe[0]:
-                                            nameList.append(hehe[0])
-                                            contentList.append(hehe[1])
-                    i = i + 1
-                    ree = True
-        else:
-            if not ree:
-                try:
-                    with open(inputFile, 'r') as input:
-                        input = input.read()
-                        input = input.split('\n')
-                except:
-                    raise(FileNotFoundError)
-                #doneAlready = []
-                tryExcept = []
-                nameList = []
-                contentList = []
-                i = 0
-
-                for x in input:
-                    kek = x
-                    if ":" in x:
-                        # if i not in doneAlready:
-                        #     print("Proccessing line: " +  str(i))
-                        # doneAlready.append(i)
-                        try:
-                            hehe = kek.split(': ', 3)
-                            tryExcept.append(hehe[1])
-                            works = True
-                            tryExcept = []
-                        except IndexError:
-                            works = False
-
-                        if works:
-                            if not "Death" in hehe[0]:
-                                if not "has requested to dock" in hehe[1]:
-                                    if not "Options" in hehe[0]:
-                                        if not "Tip" in hehe[0]:
-                                            nameList.append(hehe[0])
-                                            contentList.append(hehe[1])
-                    i = i + 1
-                    ree = True
-    else:
-        if not boolean:
-            if not ree:
-                try:
-                    with open(inputFile, 'r') as input:
-                        input = input.read()
-                        input = input.split('\n')
-                except:
-                    raise(FileNotFoundError)
-                #doneAlready = []
-                tryExcept = []
-                nameList = []
-                contentList = []
-                i = 0
-
-                for x in input:
-                    kek = x[22:]
-                    if ":" in kek:
-                        # if i not in doneAlready:
-                        #     print("Proccessing line: " +  str(i))
-                        # doneAlready.append(i)
-                        try:
-                            hehe = kek.split(': ', 1)
-                            tryExcept.append(hehe[1])
-                            works = True
-                        except IndexError:
-                            works = False
-
-                        if works:
-                            if not hehe[0] == "Death":
-                                if not " " in hehe[0]:
-                                    nameList.append(hehe[0])
-                                    contentList.append(hehe[1])
-                    i = i + 1
-                    ree = True
-            else:
-                try:
-                    with open(inputFile, 'r') as input:
-                        input = input.read()
-                        input = input.split('\n')
-                except:
-                    raise(FileNotFoundError)
-                #doneAlready = []
-                tryExcept = []
-                nameList = []
-                contentList = []
-                i = 0
-
-                for x in input:
-                    kek = x
-                    if ":" in x:
-                        # if i not in doneAlready:
-                        #     print("Proccessing line: " +  str(i))
-                        # doneAlready.append(i)
-                        try:
-                            hehe = kek.split(': ', 3)
-                            tryExcept.append(hehe[1])
-                            works = True
-                            tryExcept = []
-                        except IndexError:
-                            works = False
-
-                        if works:
-                            if not "Death" in hehe[0]:
-                                if not "has requested to dock" in hehe[1]:
-                                    if not "Options" in hehe[0]:
-                                        if not "Tip" in hehe[0]:
-                                            nameList.append(hehe[0])
-                                            contentList.append(hehe[1])
-                    i = i + 1
-                    ree = True
-        else:
-            if not ree:
-                try:
-                    with open(inputFile, 'r') as input:
-                        input = input.read()
-                        input = input.split('\n')
-                except:
-                    raise(FileNotFoundError)
-                #doneAlready = []
-                tryExcept = []
-                nameList = []
-                contentList = []
-                i = 0
-
-                for x in input:
-                    kek = x
-                    if ":" in kek:
-                        # if i not in doneAlready:
-                        #     print("Proccessing line: " +  str(i))
-                        # doneAlready.append(i)
-                        try:
-                            hehe = kek.split(': ', 1)
-                            tryExcept.append(hehe[1])
-                            works = True
-                        except IndexError:
-                            works = False
-
-                        if works:
-                            if not "Death" in hehe[0]:
+            if works:
+                if exclusionsPresent:
+                    ex = 0
+                    for exclusion in exclusions:
+                        if exclusion[-3:] == "[0]":
+                            if not exclusion[:-3] in hehe[0]:
+                                ex += 1
+                        elif exclusion[-3:] == "[1]":
+                            if not exclusion[:-3] in hehe[1]:
+                                ex += 1
+                        elif exclusion[-3:] == "[2]":
+                            if not exclusion[:-3] in hehe[0]:
+                                if not exclusion[:-3] in hehe[1]:
+                                    ex += 1
+                        elif exclusion[-3:] == "[3]":
+                            if not exclusion[:-3] in hehe[1][:len(exclusion)]:
+                                ex += 1
+                        else:
+                            print("Invalid exclusion formatting. Each exclusion must end with either '[0]', '[1]', '[2]' or '[3]'. \n   [0] = Exclude from Name List \n   [1] = Exclude from Content List \n   [2] = Exclude from both Lists \n   [3] = Exclude if at the beginning of Content List. Used for ooRP messages.")
+                            os._exit(0)
+                        if ex >= len(exclusions):
+                            nameList.append(hehe[0])
+                            contentList.append(hehe[1])
+                else:
+                    if not "Death" in hehe[0]:
+                        if not "has requested to dock" in hehe[1]:
+                            if not "Options" in hehe[0]:
                                 if not "Tip" in hehe[0]:
-                                    if not "has requested to dock" in hehe[1]:
-                                        nameList.append(hehe[0])
-                                        contentList.append(hehe[1])
-                    i = i + 1
-                    ree = True
-            else:
-                try:
-                    with open(inputFile, 'r') as input:
-                        input = input.read()
-                        input = input.split('\n')
-                except:
-                    raise(FileNotFoundError)
-                #doneAlready = []
-                tryExcept = []
-                nameList = []
-                contentList = []
-                i = 0
+                                    nameList.append(hehe[0])
+                                    contentList.append(hehe[1])                    
 
-                for x in input:
-                    kek = x
-                    if ":" in x:
-                        # if i not in doneAlready:
-                        #     print("Proccessing line: " +  str(i))
-                        # doneAlready.append(i)
-                        try:
-                            hehe = kek.split(': ', 3)
-                            tryExcept.append(hehe[1])
-                            works = True
-                            tryExcept = []
-                        except IndexError:
-                            works = False
-
-                        if works:
-                            if not "Death" in hehe[0]:
-                                if not "has requested to dock" in hehe[1]:
-                                    if not "Options" in hehe[0]:
-                                        if not "Tip" in hehe[0]:
-                                            nameList.append(hehe[0])
-                                            contentList.append(hehe[1])
-                    i = i + 1
-                    ree = True
+        i = i + 1
     return nameList, contentList
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -421,4 +165,5 @@ try:
 except:
     raise(FileNotFoundError)
 executionTime = (time.time() - startTime)
-print('Proccessing done.\nExecution time in seconds: ' + str(round(executionTime, 7)))
+
+print('Proccessing done.\nExecution time: ' + str(round(executionTime, 5)) + "s")
